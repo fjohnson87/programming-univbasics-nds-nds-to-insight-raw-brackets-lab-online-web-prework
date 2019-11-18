6d9b1c682559375b7aaef50d18db8b24d8b9e577
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-18 23:55:44 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -5,4 +5,12 @@ def directors_database
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
 
-def directors_totals
+directors_totals = "Stephen Spielberg"=>1357566430,
+        "Russo Brothers"=>2281002470,
+        "James Cameron"=>2571020373,
+        "Spike Lee"=>256624217,
+        "Wachowski Siblings"=>806180282,
+        "Robert Zemeckis"=>1273838385,
+        "Quentin Tarantino"=>662738268,
+        "Martin Scorsese"=>636812242,
+        "Francis Ford Coppola"=>509719258
